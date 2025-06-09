bad4670da8d0c3ff78c952d70620cbb474b7dadd
EdoardoGiancarli <edo.giancarli@gmail.com>
2025-06-10 00:38:31 +0200

    Added dict2df method

--- darksun/analyze.py
+++ darksun/analyze.py
@@ -2,6 +2,8 @@
 IROS output data management and computation.
 """
 
+from pandas import DataFrame
+
 from .data import Log
 
 __all__ = [""]
@@ -20,6 +22,27 @@ def IROS_sources_log() -> Log:
     """
     raise NotImplementedError
 
+
+
+
+
+
+def dict2df(data: dict) -> DataFrame:
+    """
+    Converts `Log` database to a Pandas dataframe.
+
+    Args:
+        data (dict): Input data.
+    
+    Returns:
+        df (DataFrame): Output dataframe.
+    """
+    df = DataFrame({
+        (cam, param): values
+        for cam, cam_data in data.items() 
+        for param, values in cam_data.items()
+    })
+    return df
 
 
 # end
