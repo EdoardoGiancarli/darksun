927840133a5fcab6da85bc5a5795875ded88208f
EdoardoGiancarli <edo.giancarli@gmail.com>
2025-07-10 11:02:05 +0200

    Inserted Log name

--- darksun/data.py
+++ darksun/data.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from copy import deepcopy
 
 import numpy as np
-import pandas as pd
+from pandas import DataFrame
 from astropy.io.fits.fitsrec import FITS_rec
 from astropy.coordinates import SkyCoord
 from astropy.wcs import WCS
@@ -41,8 +41,11 @@ class Log:
             Log structure (initialized to `None`).
         params (Sequence[LogEntry] | None):
             Log parameter entries (initialized to `None`).
+        name (str | None, optional (default=`None`)):
+            Log name.
     """
-    def __init__(self) -> None:
+    def __init__(self, name: str | None = None) -> None:
+        self.name = f"{name.upper()} Log" if name else None
         self._log = None
         self._params = None
     
@@ -102,7 +105,7 @@ class Log:
             self._log[entry.entry] = []
         self._params += entries
     
-    def to_dataframe(self) -> pd.DataFrame:
+    def to_dataframe(self) -> DataFrame:
         """
         Converts the dict Log to a Pandas DataFrame.
 
@@ -110,10 +113,13 @@ class Log:
             output (DataFrame):
                 Deepcopy of the Log converted to DataFrame.
         """
-        return pd.DataFrame(deepcopy(self._log))
+        return DataFrame(deepcopy(self._log))
 
 
-def create_log(params: LogEntry | Sequence[LogEntry]) -> Log:
+def create_log(
+    params: LogEntry | Sequence[LogEntry],
+    name: str | None = None,
+) -> Log:
     """
     Initializes a Log instance with the given parameters to manage
     the logging of the IROS procedure for the WFM cameras.
@@ -121,12 +127,14 @@ def create_log(params: LogEntry | Sequence[LogEntry]) -> Log:
     Args:
         params (LogEntry | Sequence[LogEntry]):
             Sequence with the specified parameter entries.
+        name (str | None, optional (default=`None`)):
+            Log name.
 
     Returns:
         output (Log):
             Log instance containing the initialized log structure.
     """
-    log = Log()
+    log = Log(name)
     log.initialize(params)
     return log
 
@@ -187,11 +195,11 @@ def get_data(
     Args:
         filepath (Path):
             Path to the FITS file.
-        E_min (int | float | None, optional (default=None)):
+        E_min (int | float | None, optional (default=`None`)):
             Minimum photons energy in [keV] for the data filtering.
-        E_max (int | float | None, optional (default=None)):
+        E_max (int | float | None, optional (default=`None`)):
             Maximum photons energy in [keV] for the data filtering.
-        coords (CoordEquatorial | Sequence[CoordEquatorial] | None, optional (default=None)):
+        coords (CoordEquatorial | Sequence[CoordEquatorial] | None, optional (default=`None`)):
             Input photons RA/Dec in [deg] to filter out.
 
     Returns:
@@ -266,26 +274,26 @@ def get_catalogue(
     Args:
         filepath (Path):
             Path to the FITS file.
-        n (int | tuple[int, int] | None, optional (default=None)):
+        n (int | tuple[int, int] | None, optional (default=`None`)):
             Filtered interval of sources, up to the n-th brightest
             source or from `n[0]` to `n[1]` if `n` is a tuple.
-        flux_range (tuple[int | float | None, int | float | None] | None, optional (default=None)):
+        flux_range (tuple[int | float | None, int | float | None] | None, optional (default=`None`)):
             Flux range in ph/cm2/s for the data filtering. The
             input tuple is interpreted as (`F_min`, `F_max`).
 
     Returns:
         output (CatalogueLoader):
-            CatalogueLoader instance with filterable sources catalog.
+            CatalogueLoader instance with filterable sources catalogue.
     
     Raises:
-        ValueError: If `n` or `flux_range` are both specified for catalogs filtering.
+        ValueError: If `n` or `flux_range` are both specified for catalogues filtering.
     """
     if not isinstance(filepath, Path):
         filepath = Path(filepath)
     
     if _exists_valid(filepath):
         if n and flux_range:
-            raise ValueError("Specify either 'n' or 'flux_range' to filter the catalog.")
+            raise ValueError("Specify either 'n' or 'flux_range' to filter the catalogue.")
         if flux_range is None:
             flux_range = (None, None)
         
@@ -301,7 +309,7 @@ def get_catalogue(
 def fit_WCS(
     camera: CodedMaskCamera,
     sdl: DataLoader,
-    pixels: list[tuple[int, int]] = None,
+    pixels: list[tuple[int, int]] | None = None,
     grid_step: int = 200,
 ) -> WCS:
     """
@@ -313,9 +321,9 @@ def fit_WCS(
             CodedMaskCamera instance used for imaging and reconstruction.
         sdl (DataLoader):
             DataLoader instance for the given camera.
-        pixels (list[tuple[int, int]], optional (default=None)):
+        pixels (list[tuple[int, int]], optional (default=`None`)):
             List of pixels position (row, col) for the WCS fit.
-        grid_step (int, optional (default=200)):
+        grid_step (int, optional (default=`200`)):
             Sky grid points along each axis for computing the WCS fit.
     
     Returns:
